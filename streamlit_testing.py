be351325200449e3f0a4739ad6e40e87b299651f
Tim Youell <timyouellservian@Tims-MacBook-Pro.local>
2022-08-05 15:36:17 +1000

    Updated webpage to include basic text and button

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
 
-st.set_page_config(page_title='Tims First Website', page_icon=':tada:', layout='wide')
-st.title('Hello World')
+st.set_page_config(page_title='Tims First Website', page_icon=':sloth:', layout='wide')
+st.title('Hello and welcome to Tim\'s first website!')
+st.write('Please check back soon for updates and Fantasy Premier League Football tips and stats.')
+
+st.write('[Personal Github Page](https://github.com/TimYouell15)')
+
+if st.button('Click me for a joke'):
+	st.write('The change in weather here in Sydney has got me swapping from aerosol deodorant.')
+	st.write('Roll on next week.')
+else:
+	st.write('I promise it will be worth it')
+
 
